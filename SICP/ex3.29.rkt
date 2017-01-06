3ab4ab389616a33ad2a367811e038004e5397d36
asdemon <asdemon235b@gmail.com>
2017-01-06 21:28:44 +0800

    add to ex3.30

---
+++ SICP/ex3.29.rkt
@@ -0,0 +1,46 @@
+#lang sicp
+(#%require (only racket/base error))
+
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! input invert-input) 'ok)
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay
+       and-gate-delay
+       (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay
+       or-gate-delay
+       (lambda () (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+;delay is 2*inverter-delay + and-gate-delay
+(define (or-gate-link a1 a2 output)
+ (let ((c (make-wire)) (d (make-wire)) (e (make-wire)))
+   (inverter a1 c)
+   (inverter a2 d)
+   (and-gate c d e)
+   (inverter e output)
+  'ok))
