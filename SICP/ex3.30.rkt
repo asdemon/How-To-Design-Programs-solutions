14837186fa7953ed98a249c3a5f1ddc2ed1e3579
asdemon <asdemon235b@gmail.com>
2017-01-07 20:41:04 +0800

    add to 3.36

--- SICP/ex3.30.rkt
+++ SICP/ex3.30.rkt
@@ -1,6 +1,62 @@
 #lang sicp
 (#%require (only racket/base error))
 
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures
+            (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation: WIRE" m))))
+    dispatch))
+
+(define (get-signal wire) (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+(define (call-each procedures)
+  (map (lambda (f) (f)) procedures))
+
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name) (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+
 
 (define (inverter input output)
   (define (invert-input)
@@ -23,6 +79,9 @@
   (add-action! a1 and-action-procedure)
   (add-action! a2 and-action-procedure)
   'ok)
+(define (logical-and a b)
+  (cond ((and (= a 1) (= b 1)) 1)
+        (else 0)))
 
 
 (define (or-gate a1 a2 output)
@@ -35,6 +94,9 @@
   (add-action! a1 or-action-procedure)
   (add-action! a2 or-action-procedure)
   'ok)
+(define (logical-or a b)
+  (cond ((and (= a 0) (= b 0)) 0)
+        (else 1)))
 
 ;delay is 2*inverter-delay + and-gate-delay
 (define (or-gate-link a1 a2 output)
@@ -61,6 +123,7 @@
     (or-gate c1 c2 c-out)
     'ok))
 
+;ex3.30
 (define (ripple-carry-adder ak bk sk c)
   (cond ((= (length ak) 1) (let ((c0 (make-wire)))
                              (begin (set-signal! c0 0)
@@ -68,6 +131,64 @@
         (else (let ((c1 (make-wire)))
                 (ripple-carry-adder (cdr ak) (cdr bk) (cdr sk) c1)
                 (full-adder (car ak) (car bk) c1 (car sk) c)))))
-                                 
-      
-  
+
+
+;agenda
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty: FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda
+                           (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
---
+++ SICP/ex3.31.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+;the procedure is immediately run, it is necessary to keep the internal state coherent
+;when the procedure is not immediately run, the internal state is not consistent, and the
+; output can be wrong
